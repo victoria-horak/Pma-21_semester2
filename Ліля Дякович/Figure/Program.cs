a587b22ec533a42fcc765bff764151fe877c60a6
LiliaDiak <96654700+LiliaDiak@users.noreply.github.com>
2023-02-15 20:05:13 +0200

    Add files via upload

---
+++ Ліля Дякович/Figure/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rec = new Rectangle(2, 4);
+            rec.print();
+            Console.WriteLine();
+            Circle cir = new Circle(3);
+            cir.print();
+            Console.WriteLine();
+            Square square = new Square(5);
+            square.print();
+
+        }
+    }
+}
---
+++ Ліля Дякович/Figure/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    class Rectangle: Figure
+    {
+        private double secondSide;
+        public Rectangle(double secondSide, double side):base(side)
+        {
+            this.secondSide = secondSide;
+        }
+
+        public override double area()
+        {
+            return side * secondSide;
+
+        }
+
+        public override double perimetr()
+        {
+            return side * 2 + secondSide * 2;
+        }
+
+        public override void print()
+        {
+            Console.WriteLine("Perimetr: " + perimetr() + " Square: " + area());
+        }
+    }
+}
