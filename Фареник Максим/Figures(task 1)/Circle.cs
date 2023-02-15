4a26a2510913d60985dd951ec4915e0b14e88c9b
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2023-02-15 21:53:16 +0200

    Add files via upload

---
+++ Фареник Максим/Figures(task 1)/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    internal class Circle:Shape
+    {
+        public Circle()
+        {
+            side = 0;
+        }
+        public Circle(double side)
+        {
+            this.side = side;
+        }
+        public override double getPerimeter()
+        {
+            double perimeter = 2.0 * 3.14 * side;
+            return perimeter;
+        }
+        public override double getArea()
+        {
+            double area = 3.14 * side * side;
+            return area;
+        }
+        public override void Output()
+        {
+            Console.WriteLine("Circle: Radius is " + side + ", Perimeter is " + getPerimeter() + ", Area is " + getArea() + "\n");
+        }
+
+    }
+}
---
+++ Фареник Максим/Figures(task 1)/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    internal class Rectangle : Shape
+    {
+        public double secondSide;
+        public Rectangle()
+        {
+            side = 0;
+            secondSide = 0;
+        }
+        public Rectangle(double side, double secondSide)
+        {
+            this.side = side;
+            this.secondSide = secondSide;
+        }
+        public override double getPerimeter()
+        {
+            double perimeter = (secondSide + side) * 2;
+            return perimeter;
+        }
+        public override double getArea()
+        {
+            double area = secondSide * side;
+            return area;
+        }
+        public override void Output()
+        {
+            Console.WriteLine("Rectangle: First side is " + side + ", Second side is " + secondSide + ", Perimeter is " + getPerimeter() + ", Area is " + getArea() + "\n");
+        }
+
+    }
+}
