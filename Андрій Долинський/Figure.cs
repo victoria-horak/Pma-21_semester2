72e6a56558e6099171f3bb8c68c75d30ae3565e4
andrewdolynskyi <113112751+andrewdolynskyi@users.noreply.github.com>
2023-02-15 21:21:46 +0200

    Add files via upload

---
+++ Андрій Долинський/Figure.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Shapes
+{
+    abstract class Figure : IArea, IPerimeter
+    {
+        protected double side;
+
+        public Figure(double side)
+        {
+            this.side = side;
+        }
+        public abstract double Perimetr();
+        public abstract double Area();
+        public abstract void Print();
+    }
+}
---
+++ Андрій Долинський/Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Shapes
+{
+    class Square : Rectangle
+    {
+        public Square(double side) : base(side, side) { }
+
+        public override double Area()
+        {
+            return side * side;
+        }
+
+        public override double Perimetr()
+        {
+            return 4 * side;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Perimeter of figure:: " + Perimetr() + " Area of figure:: " + Area());
+        }
+    }
+}
