9d08f7e225d5fbd3ba5b6226ba670f0712f2d328
VolodymyrPinyaha23 <113587271+VolodymyrPinyaha23@users.noreply.github.com>
2023-02-15 23:03:24 +0200

    Add files via upload

---
+++ Піняга Володимир/Task1/First_Solution_on_Csharp/First_Project_on_Csharp/IArea.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace First_Project_on_Csharp
+{
+    internal interface IArea
+    {
+        double Square();
+    }
+}
---
+++ Піняга Володимир/Task1/First_Solution_on_Csharp/First_Project_on_Csharp/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace First_Project_on_Csharp
+{
+    abstract class Shape: IArea, IPerimeter
+    {
+        public double side1; //харатеристика
+        public abstract double Square();
+        public abstract double Perimeter();
+        public abstract void print();
+    }
+}
