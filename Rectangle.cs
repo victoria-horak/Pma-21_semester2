1e1989b826bceb0808972074f97e98950b2b5ffe
victoria-horak <113350889+victoria-horak@users.noreply.github.com>
2023-02-16 18:27:01 +0200

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+     class Rectangle : Figura
+    {
+        private double width;
+        private double height;
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public override double GetArea()
+        {
+            double area = width * height;
+            return area;
+        }
+        public override double GetPerimeter()
+        {
+            double perimeter = 2 * width + 2 * height;
+            return perimeter;
+        }
+    }
+}
