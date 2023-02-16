8bd4666789508af3b2c064a2e04457f9fcab2848
Vitaliy <113024338+vitalikkostiuk7@users.noreply.github.com>
2023-02-16 17:21:16 +0200

    Add files via upload

---
+++ Костюк В/Area.cs
@@ -0,0 +1,7 @@
+﻿namespace Figures
+{
+    public interface ISquare
+    {
+        double area();
+    }
+}
---
+++ Костюк В/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Figures
+{
+    class Circle : Figure
+    {
+        public Circle(double radius) : base(radius) { }
+
+        public override double area() => Math.Round((Math.PI * side * side), 3);
+
+        public override double perimetr() => Math.Round((2 * Math.PI * side), 3);
+
+        public override void print() => Console.WriteLine("Circle: Length = " + perimetr() + "| Square = " + area());
+    }
+}
