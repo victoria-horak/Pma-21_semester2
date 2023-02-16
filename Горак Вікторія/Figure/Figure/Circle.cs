e62e509347ce306c8556d43a0ca7684e25f87e37
victoria-horak <113350889+victoria-horak@users.noreply.github.com>
2023-02-16 18:36:20 +0200

    Add files via upload

---
+++ Горак Вікторія/Figure/Figure/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    class Circle: Figura
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double GetArea()
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+        public override double GetPerimeter()
+        {
+            double perimeter = 2 * Math.PI * radius;
+            return perimeter;
+         
+        }
+    }
+    
+
+}
---
+++ Горак Вікторія/Figure/Figure/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    class Square: Rectangle
+    {
+      
+        private double side_a;
+       
+        public Square(double side_a) : base(side_a, side_a)
+        { 
+            this.side_a = side_a; 
+        }
+
+        public override double GetArea()
+        {
+            double area = side_a * side_a;
+            return area;
+
+        }
+        public override double GetPerimeter()
+        {
+            double perimeter = side_a * 4;
+            return perimeter;
+        }
+
+    }
+}
