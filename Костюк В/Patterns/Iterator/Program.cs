ea131cf2d10c231e20416e3fd780d4db38e2e5ae
Vitaliy <113024338+vitalikkostiuk7@users.noreply.github.com>
2023-02-21 21:15:17 +0200

    Add files via upload

---
+++ Костюк В/Patterns/Iterator/Program.cs
@@ -0,0 +1,11 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        string file = "employees.txt";
+        IIterator<Employee> iterator = new Iterator.EmployeeFileIterator(file);
+        Iterator.EmployeeProcessor processor = new Iterator.EmployeeProcessor(iterator);
+        processor.Process();
+    }
+}
