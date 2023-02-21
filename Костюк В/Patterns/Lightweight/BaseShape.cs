0d945748b9c7468f2d638e1c0fd43dee09212366
Vitaliy <113024338+vitalikkostiuk7@users.noreply.github.com>
2023-02-21 21:18:51 +0200

    Add files via upload

---
+++ Костюк В/Patterns/Lightweight/BaseShape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lightweight
+{
+    public class BaseShape
+    {
+        private int x;
+        private int y;
+        private int size;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Size { get => size; set => size = value; }
+
+        public BaseShape(int x, int y, int size)
+        {
+            X = x;
+            Y = y;
+            Size = size;
+        }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine($"Drawing base shape at ({X} {Y}) with size {Size}");
+        }
+    }
+}
---
+++ Костюк В/Patterns/Lightweight/Program.cs
@@ -0,0 +1,10 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    
+    {
+        Lightweight.LightShape square = new Lightweight.LightShape(0, 0, 5, "Blue");
+        square.Draw();
+    }
+}
