d008274fde03064aa64ee4e9ca544ded0ef5dfdb
Yurik <96499300+Yura162@users.noreply.github.com>
2023-02-21 17:16:12 +0200

    Add files via upload

---
+++ Кордіяка Юрій/Figura/Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Adapter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double celsius = 33;
+            double fahrenheit = 33;
+            IDegrees dcelsius = new DegreesCelsius(celsius);
+            IDegrees dfahrenheit = new AdapterForDegreesFahrenheit(new DegreesFahrenheit(fahrenheit));
+            Console.WriteLine(dcelsius.GetDegrees());
+            Console.WriteLine(dfahrenheit.GetDegrees());
+        }
+    }
+}
