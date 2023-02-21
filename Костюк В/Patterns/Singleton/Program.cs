81c1e41fc2d9db1e1f64e75351d8e4611e7d39b8
Vitaliy <113024338+vitalikkostiuk7@users.noreply.github.com>
2023-02-21 21:16:17 +0200

    Add files via upload

---
+++ Костюк В/Patterns/Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = Singleton.LogWriter.GetInstance();
+        logger.WriteToGradebook("Vitaliy", "Proga", 5);
+        logger.WriteToGradebook("Taras", "Eng", 4);
+    }
+}
