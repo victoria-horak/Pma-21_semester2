d64a226724dd3e410daa632494bf551a2bc2ffe4
LiliaDiak <96654700+LiliaDiak@users.noreply.github.com>
2023-02-21 17:26:09 +0200

    Add files via upload

---
+++ Ліля Дякович/Patterns/ChainOfResponsibility/Translator.cs
@@ -0,0 +1,11 @@
+﻿abstract class Translator
+{
+    protected Translator next;
+
+    public void SetNext(Translator translator)
+    {
+        next = translator;
+    }
+
+    public abstract string Translate(string fromLanguage, string toLanguage);
+}
---
+++ Ліля Дякович/Patterns/ChainOfResponsibility/UkToEng.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfRes
+{
+    class UkrainToEnglishTranslator : Translator
+    {
+        public override string Translate( string fromLanguage, string toLanguage)
+        {
+            if (fromLanguage == "Ukrain" && toLanguage == "English")
+            {
+                return this + " can translate";
+            }
+            else if (next != null)
+            {
+                System.Console.WriteLine(this + " cannot translate this text");
+                return next.Translate( fromLanguage, toLanguage);
+            }
+            else
+            {
+                return "Cant translate.";
+            }
+        }
+    }
+}
