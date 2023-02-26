19c0a7243b87a7b91b2662f45242a936872bb0b3
VolodymyrPinyaha23 <113587271+VolodymyrPinyaha23@users.noreply.github.com>
2023-02-26 19:28:02 +0200

    Add files via upload

--- Піняга Володимир/First_Solution_on_Csharp/First_Project_on_Csharp/Program.cs
+++ Піняга Володимир/First_Solution_on_Csharp/First_Project_on_Csharp/Program.cs
@@ -1,24 +1,18 @@
 ﻿using System;
 
-
-namespace First_Project_on_Csharp 
-{
-    public class Program
+namespace Prototype_Pattern
+{ 
+    internal class Program
     {
-        public static void Main()
+        static void Main()
         {
-            Rectangle rectangle = new Rectangle(2, 5);
-            rectangle.print();
-
-            Circle circle = new Circle(8);
-            circle.print();
-
-            Square square = new Square(6);
-            square.print();
+            IRobot robotDonor = new Terminator();
+            robotDonor.SetName("T-800");
+            IRobot robotClone = robotDonor.clone();
+            robotClone.SetName("T-Rex");
+            Console.WriteLine("The original: " + robotDonor.GetName());
+            Console.WriteLine("The Clone: " + robotClone.GetName());
 
         }
     }
-   
 }
-
-
