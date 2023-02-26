4ea16def414b20037dde6a559433c67e0e5a909b
VolodymyrPinyaha23 <113587271+VolodymyrPinyaha23@users.noreply.github.com>
2023-02-26 19:32:49 +0200

    Add files via upload

---
+++ Піняга Володимир/First_Solution_on_Csharp/First_Project_on_Csharp/Prototype_Pattern/Terminator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prototype_Pattern
+{
+    class Terminator : IRobot
+    {
+        private string name;
+        public Terminator() { }
+        public Terminator(string _name) { name = _name; }
+        public string GetName() => name;
+        public void SetName(string name) { this.name = name; }
+        public IRobot clone()
+        {
+            return (IRobot)this.MemberwiseClone();
+        }
+    }
+}
