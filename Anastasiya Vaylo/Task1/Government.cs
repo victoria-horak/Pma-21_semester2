893089b3a768ed8da82b52fcbbf0de7a648cab7d
anastasiyavaylo <113297968+anastasiyavaylo@users.noreply.github.com>
2023-02-28 11:36:24 +0200

    Add files via upload

---
+++ Anastasiya Vaylo/Task1/Government.cs
@@ -0,0 +1,32 @@
+﻿
+namespace singl
+{
+    public class Government
+    {
+        private Government() { }
+        private static Government _government;
+        private int _budget = 0;
+
+        public static Government GetInstance()
+        {
+            if (_government == null)
+            {
+                _government = new Government();
+            }
+            return _government;
+        }
+        public void IncreaseBudget()
+        {
+            _budget = _budget + 10;
+        }
+        public void DecreaseBudget()
+        {
+            _budget = _budget - 10;
+        }
+        public int GetBudget()
+        {
+            return _budget;
+        }
+
+    }
+}
---
+++ Anastasiya Vaylo/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace singl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Government a = Government.GetInstance();
+            Government b = Government.GetInstance();
+
+            a.IncreaseBudget();
+            a.IncreaseBudget();
+            int x = b.GetBudget();
+            System.Console.WriteLine("The budget is " + x );
+        }
+    }
+}
