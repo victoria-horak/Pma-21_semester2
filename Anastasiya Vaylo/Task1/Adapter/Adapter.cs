0cafa5984640f5d01ffbe3f2286ca2819abb71c2
anastasiyavaylo <113297968+anastasiyavaylo@users.noreply.github.com>
2023-02-28 11:42:12 +0200

    Add files via upload

---
+++ Anastasiya Vaylo/Task1/Adapter/Adapter.cs
@@ -0,0 +1,39 @@
+﻿namespace Adapter
+{
+  
+    interface ICreature
+    {
+        public void Eat();
+    }
+    class HumanAdapter : Human, ICreature
+    {
+        public void Eat()
+        {
+            Lunch();
+        }
+    }
+
+    class Human
+    {
+        public void Lunch()
+        {
+            System.Console.WriteLine("Eat lunch with knife and fork");
+        }
+    }
+
+    class AnimalAdapter : Animal, ICreature
+    {
+        public void Eat()
+        {
+            Devour();
+        }
+    }
+
+    class Animal
+    {
+        public void Devour()
+        {
+            System.Console.WriteLine("Eat something from the ground with mouth");
+        }
+    }
+}
---
+++ Anastasiya Vaylo/Task1/Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Adapter
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            HumanAdapter adapter1 = new HumanAdapter();
+            adapter1.Eat();
+
+            AnimalAdapter adapter2 = new AnimalAdapter();
+            adapter2.Eat();
+
+        }
+    }
+}
