8464249e25e05df1c8a1181ac63f68fb24a05b70
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2023-03-01 00:45:22 +0200

    Update Program.cs

--- Фареник Максим/Patterns (task 2)/Factory Method/Program.cs
+++ Фареник Максим/Patterns (task 2)/Factory Method/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,24 +12,9 @@ namespace Factory_Method
         {
             Console.WriteLine("What do you want to drink coffee, tea or cocoa?");
             string input = Console.ReadLine();
-            IDrinkFactory factory;
-            switch (input)
-            {
-                case "coffee":
-                    factory = new CoffeeFactory();
-                    break;
-                case "tea":
-                    factory = new TeaFactory();
-                    break;
-                case "cocoa":
-                    factory = new CocoaFactory();
-                    break;
-                default:
-                    Console.WriteLine("Incorrect input");
-                    return;
-            }
-            IDrink drink = factory.CreateDrink();
-            drink.Prepare();
+            Client client = new Client();
+            client.orderDrink(input);
+
         }
     }
 }
