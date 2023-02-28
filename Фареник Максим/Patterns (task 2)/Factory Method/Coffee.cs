2e9b722c1cacfe2d2927b1877ecb33874d76eb74
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2023-02-28 16:03:28 +0200

    Add files via upload

---
+++ Фареник Максим/Patterns (task 2)/Factory Method/Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    internal class Coffee: IDrink
+    {
+        public void Prepare()
+        {
+            Console.WriteLine("Preparing a coffee...\n");
+        }
+    }
+}
---
+++ Фареник Максим/Patterns (task 2)/State/BookedState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    internal class BookedState: State
+    {
+        public BookedState(TicketBooking context) : base(context)
+        {
+        }
+
+        public override string BookTicket(string passengerName)
+        {
+            return $"Ticket already booked for {context.GetPassengersList()[0]}";
+        }
+
+        public override string CancelBooking(string passengerName)
+        {
+            if (context.GetPassengersList()[0] == passengerName)
+            {
+                context.GetPassengersList().Remove(passengerName);
+                context.ChangeState(new AvailableState(context));
+                return $"Booking cancelled for {passengerName}";
+            }
+            else
+            {
+                return $"Cannot cancel booking for {passengerName}. Booking is held by {context.GetPassengersList()[0]}";
+            }
+        }
+    }
+}
