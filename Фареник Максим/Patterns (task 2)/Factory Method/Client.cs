f0e01d22e388a625283f184d4e1f96cf59c0bc3e
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2023-03-01 00:46:20 +0200

    Add files via upload

---
+++ Фареник Максим/Patterns (task 2)/Factory Method/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    internal class Client
+    {
+        IDrinkFactory factory;
+        public void orderDrink(string input)
+        {
+
+            if (input == "coffee")
+                factory = new CoffeeFactory();
+            else if (input == "tea")
+                factory = new TeaFactory();
+            else if (input == "cocoa")
+                factory = new CocoaFactory();
+            else
+                Console.WriteLine("Incorrect value");
+
+
+            IDrink drink = factory.CreateDrink();
+            drink.Prepare();
+        }
+
+
+    }
+}
