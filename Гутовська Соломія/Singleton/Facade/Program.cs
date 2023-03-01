ef78efe5ba94a1478dcc2d8ac2237edcc224abf4
solomiiahutovska <113610904+solomiiahutovska@users.noreply.github.com>
2023-03-01 02:40:24 +0200

    Create Program.cs

---
+++ Гутовська Соломія/Singleton/Facade/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var restaurant = new RestaurantFacade();
+            var client = new Client(restaurant);
+
+            client.OrderMeal("Caesar salad", "Chocolate cake", "Water");
+        }
+    }
+}
