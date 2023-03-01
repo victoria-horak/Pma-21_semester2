93f810cc53d1ce800babf3a4abf1078966c8ca8c
solomiiahutovska <113610904+solomiiahutovska@users.noreply.github.com>
2023-03-01 09:54:56 +0200

    Create IStrategy.cs

---
+++ Гутовська Соломія/Singleton/Strategy/IStrategy.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public interface IStrategy
+    {
+        void Attack();
+    }
+}
