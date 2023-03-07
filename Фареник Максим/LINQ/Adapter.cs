f0f74d63f3148904aa527bf537ecf53836094f23
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2023-03-07 20:51:28 +0200

    Add files via upload

---
+++ Фареник Максим/LINQ/Adapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Adapter
+    {
+        private Document forAdapting;
+
+        public Adapter(Document forAdapting)
+        {
+            this.forAdapting = forAdapting;
+        }
+        public FilteredType Adapting()
+        {
+            return new FilteredType
+            {
+                identificatorId = forAdapting.identificatorId,
+                objectId = forAdapting.dataItem.objectId,
+                effectiveFrom = DateTime.Parse(forAdapting.dataItem.effectiveFrom),
+                effectiveTo = DateTime.Parse(forAdapting.dataItem.effectiveTo),
+                type = forAdapting.info.type
+            };
+        }
+    }
+}
---
+++ Фареник Максим/LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstFile = @"D:\C# projects\file.json";
+            string information = File.ReadAllText(firstFile);
+
+            string fileToWrite = @"D:\C# projects\output.json";
+            string date = @"D:\C# projects\date.txt";
+            var dateCheck = DateTime.Parse(File.ReadAllText(date));
+        
+            var documents = JsonConvert.DeserializeObject<List<Document>>(information).Select(element => new Adapter(element).Adapting()).Where(element => element.effectiveFrom <= dateCheck && element.effectiveTo >= dateCheck);
+         
+            foreach (var iterator in documents)
+            {
+                Console.WriteLine(iterator.ToString()+"\n");     
+            }
+
+            File.WriteAllText(fileToWrite, JsonConvert.SerializeObject(documents));
+        }
+    }
+}
