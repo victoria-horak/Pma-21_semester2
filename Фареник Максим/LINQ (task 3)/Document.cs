dbda84edc9ea80f20f4d64ffffde7149beca38eb
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2023-03-07 20:52:31 +0200

    Add files via upload

---
+++ Фареник Максим/LINQ (task 3)/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Document
+    {
+        public string identificatorId { get; set; }
+        public DataItem dataItem { get; set; }
+        public Info info { get; set; }
+    }
+}
