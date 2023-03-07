fa4e71b31324ba35650135b3bfd9e0659bd13be2
Vitaliy <113024338+vitalikkostiuk7@users.noreply.github.com>
2023-03-07 23:50:14 +0200

    Add files via upload

---
+++ Костюк В/Json/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace json_read_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("file.json");
+            string output = "jsonOutput.json";
+
+            List<User> users= new List<User>();
+            JArray jsonArray = JArray.Parse(json);
+            foreach (JObject item in jsonArray)
+            {
+                User currUser = new User();
+                currUser.identificatorId = (string)item["identificatorId"];
+                currUser.effectiveFrom = DateTime.Parse((string)item["dataItem"]["effectimeFrom"]);
+                currUser.effectiveTo = DateTime.Parse((string)item["dataItem"]["effectiveTo"]);
+
+                users.Add(currUser);
+            }
+            var dateToCheck = DateTime.Parse(File.ReadAllText("dataToCheck.txt"));         
+            var thoseWhoFit = from user in users
+                              where user.effectiveFrom <= dateToCheck &&
+                                    user.effectiveTo >= dateToCheck
+                              select user;
+
+            foreach (User curr in thoseWhoFit)
+            {
+                Console.WriteLine(curr);
+                File.WriteAllText(output, JsonConvert.SerializeObject(thoseWhoFit, Formatting.Indented));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Костюк В/Json/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace json_read_new
+{
+    class User
+    {
+        private DateTime effectiveTo1;
+        private DateTime effectiveFrom1;
+        private string identificatorId1;
+
+        public string identificatorId { get => identificatorId1; set => identificatorId1 = value; }
+        public DateTime effectiveTo { get => effectiveTo1; set => effectiveTo1 = value; }
+        public DateTime effectiveFrom { get => effectiveFrom1; set => effectiveFrom1 = value; }
+
+        public override string ToString()
+        {
+            return $"identificatorId: {identificatorId}\neffectiveFrom: {effectiveFrom}\neffectiveTo: {effectiveTo}\n";
+        }
+    }
+}
