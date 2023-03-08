d0481625b6caa9ca9aaa9db0013d4d6fecefea58
HoroshokRuslan13436 <113587110+HoroshokRuslan13436@users.noreply.github.com>
2023-03-08 16:03:18 +0200

    Add files via upload

---
+++ Горошок Руслан/Api/APITask.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class APITask : Controller
+    {
+        [HttpGet(Name = "GetName")]
+        public string Get()
+        {
+            return "Hello , World!";
+        }
+        [HttpPost(Name = "PostName")]
+        public string Post(string name)
+        {
+            return "Hello world " + name;
+        }
+    }
+}
