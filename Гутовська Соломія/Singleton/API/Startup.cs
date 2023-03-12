5b23d0e9181158d6bdfc6c14592594419d90994f
solomiiahutovska <113610904+solomiiahutovska@users.noreply.github.com>
2023-03-12 23:28:53 +0200

    Update Startup.cs

--- Гутовська Соломія/Singleton/API/Startup.cs
+++ Гутовська Соломія/Singleton/API/Startup.cs
@@ -18,7 +18,6 @@ namespace HelloWorld
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
