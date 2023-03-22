d7c1112176d258c0104cbbc1196a9053b62ac69a
solomiiahutovska <113610904+solomiiahutovska@users.noreply.github.com>
2023-03-22 07:54:39 +0200

    Create Info.cs

---
+++ Гутовська Соломія/Singleton/LINQ/Info.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQwithJSON
+{
+    internal class Info
+    {
+        public string car { get; set; }
+        public string color { get; set; }
+        public string currentAt { get; set; }
+        public string currentFrom { get; set; }
+        public string type { get; set; }
+        public string number { get; set; }
+        public string resolution { get; set; }
+        public string size { get; set; }
+    }
+}
