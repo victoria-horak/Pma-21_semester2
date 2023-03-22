28f1822477eb6d2233ef9c69526bda92dfbfcbd1
solomiiahutovska <113610904+solomiiahutovska@users.noreply.github.com>
2023-03-22 07:55:38 +0200

    Create RequiredData.cs

---
+++ Гутовська Соломія/Singleton/LINQ/RequiredData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQwithJSON
+{
+    internal class RequiredData
+    {
+        public string identificatorId { get; set; }
+        public DataItem dataItem { get; set; }
+        public Info info { get; set; }
+    }
+}
