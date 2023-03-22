c26ab99f680ef6bb64fef403b172fcc87c85fd46
VolodymyrPinyaha23 <113587271+VolodymyrPinyaha23@users.noreply.github.com>
2023-03-22 09:35:18 +0200

    Add files via upload

---
+++ Піняга Володимир/First_Solution_on_Csharp/First_Project_on_Csharp/Figure/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace First_Project_on_Csharp
+{
+    class Rectangle : Shape
+    {
+        protected double side2;
+        public Rectangle() => side1 = side2 = 0;
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+            
+        //public Rectangle(int n) => side2 = n;
+        public override double Square() => side1 * side2;
+
+        public override double Perimeter() => (side1 + side2) * 2;
+        public override void print()
+        {
+            Console.WriteLine("Rectangle: Sides: " + side1 + ", "+side2+ " || Square = " + Square() + " || Perimeter = " + Perimeter());
+        }
+
+
+
+    }
+}
---
+++ Піняга Володимир/First_Solution_on_Csharp/First_Project_on_Csharp/Figure/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace First_Project_on_Csharp
+{
+    class Square: Rectangle
+    {
+        public Square(double side1) : base(side1, side1) { }
+        public Square(): base() { }
+        public override void print()
+        {
+            Console.WriteLine("Square: Side = " + side1 + " || Square = " + Square() + " || Perimeter = " + Perimeter());
+        }
+            
+    }
+}
