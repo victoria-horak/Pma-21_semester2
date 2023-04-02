c7810a50587446bf40c5155b8bddfa8490cba066
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2023-04-02 16:29:31 +0300

    Add files via upload

---
+++ Фареник Максим/User API (Task 5)/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp
+{
+    public class User
+    {
+
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? UserLastName { get; set; }
+    }
+}
---
+++ Фареник Максим/User API (Task 5)/UserController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Text.Json;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select UserId,UserName,UserLastName from mytestdb.MyUser";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnection");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpGet("{name}")]
+        public JsonResult Get(string name)
+        {
+            string query = $"select UserId, UserName, UserLastName from mytestdb.MyUser where UserName like '%{name}%'";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnection");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Insert(User user)
+        {
+            string query = $"INSERT INTO mytestdb.MyUser (UserName, UserLastName) VALUES('{user.UserName}', '{user.UserLastName}')";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnection");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult("User added successfully");
+        }
+        [HttpPut("{id}")]
+        public JsonResult UpdateUser(int id, User user)
+        {
+            string query = $"UPDATE mytestdb.MyUser SET UserName='{user.UserName}', UserLastName='{user.UserLastName}' WHERE UserId={id}";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnection");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult("User updated successfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = $"DELETE FROM mytestdb.MyUser WHERE UserId={id}";
+
+            string updateQuery = $"SET @cnt = 0; UPDATE mytestdb.MyUser SET UserId = @cnt:= @cnt + 1 WHERE UserId > {id}; ALTER TABLE mytestdb.MyUser AUTO_INCREMENT = @cnt;";
+
+            string sqlDataSource = _configuration.GetConnectionString("MyConnection");
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    mycommand.ExecuteNonQuery();
+                }
+                mycon.Close();
+            }
+
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand updateCommand = new MySqlCommand(updateQuery, mycon))
+                {
+                    updateCommand.ExecuteNonQuery();
+                }
+                mycon.Close();
+            }
+
+            return new JsonResult("User deleted successfully");
+        }
+    }
+}
