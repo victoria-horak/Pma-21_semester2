b85b66a5d22af42aa5a35bfec960286a49d6e9fe
Danylo <98780995+Den-Nyk@users.noreply.github.com>
2023-04-04 11:28:24 +0300

    Add files via upload

---
+++ Николайчук Данило/readDataFromJson/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+using System.Collections.Generic;
+using readDataFromJson;
+using Newtonsoft.Json;
+
+namespace readDataFromJson
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "C:\\Users\\asd\\source\\C#\\University\\readDataFromJson\\readDataFromJson\\file.json";
+            string text = File.ReadAllText(path);
+            var data = JsonConvert.DeserializeObject<List<SomeObject>>(text);
+            Console.WriteLine("All data:");
+            foreach (var ob in data)
+            {
+                Console.WriteLine(ob.ToString());
+            }
+            Console.WriteLine("Data that is okey:");
+            string datePath = @"C:\Users\asd\source\C#\University\readDataFromJson\readDataFromJson\TextFile1.txt";
+            var dateToCheck = DateTime.Parse(File.ReadAllText(datePath));
+            var newData = (from doc in data
+                           where doc.effectimeFrom <= dateToCheck && doc.effectiveTo >= dateToCheck
+                           select doc).ToList();
+
+            foreach(var ob in newData) { Console.WriteLine(ob.ToString()); }
+                         
+        }
+    }
+}
---
+++ Николайчук Данило/readDataFromJson/SomeObject.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readDataFromJson
+{
+    internal class SomeObject
+    {
+        private string identificatorId { get; set; }
+        private long objectId { get; set; }
+        public DateTime effectimeFrom { get; set; }
+        public DateTime effectiveTo { get; set; }
+        private string twoUselessFielsd { get; set; }
+        private DateTime currentAt { get; set; }
+        private DateTime currentFrom { get; set; }
+        private string type { get; set; }
+
+        public SomeObject(string identificatorId, string objectId, string effectimeFrom, string effectiveTo, string twoUselessFielsd, string currentAt, string currentFrom, string type)
+        {
+            this.identificatorId = identificatorId;
+            this.objectId = long.Parse(objectId);
+            this.effectimeFrom = DateTime.Parse(effectimeFrom);
+            this.effectiveTo = DateTime.Parse(effectiveTo);
+            this.twoUselessFielsd = twoUselessFielsd;
+            this.currentAt = DateTime.Parse(currentAt);
+            this.currentFrom = DateTime.Parse(currentFrom);
+            this.type = type;
+        }
+        public override string ToString()
+        {
+            return "Identificator id: " + identificatorId + "\neffectime from: " + effectimeFrom.ToString()
++ "\neffectime to: " + effectiveTo.ToString() + "\ntwo useless fielsd: " + twoUselessFielsd+"\n\n";
+        }
+    }
+}
