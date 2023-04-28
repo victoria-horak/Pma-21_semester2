6d0c04f42e28dfb28d9226dffba5527cb7eab3a3
MartaBilas <113312120+MartaBilas@users.noreply.github.com>
2023-04-28 22:24:28 +0300

    Add files via upload

---
+++ Білас Марта/HelloWorld/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloController : Controller
+    {
+        [Route("Hello")]
+        public IActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
