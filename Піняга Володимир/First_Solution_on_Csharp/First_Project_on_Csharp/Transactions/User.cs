a831d77f278405dadcac483b2186ad919c25b5c7
VolodymyrPinyaha23 <113587271+VolodymyrPinyaha23@users.noreply.github.com>
2023-05-21 20:18:59 +0300

    Add files via upload

---
+++ Піняга Володимир/First_Solution_on_Csharp/First_Project_on_Csharp/Transactions/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Transactions.Entities;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int? Money { get; set; }
+}
---
+++ Піняга Володимир/First_Solution_on_Csharp/First_Project_on_Csharp/Transactions/UserController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+//using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+//using RouteAttribute = System.Web.Http.RouteAttribute;
+
+
+namespace TransactionsProgram.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        string connectionString = "server=127.0.0.1;port=3306;user=root;password=df42k9@wqbnYU808;database=redis;";
+
+        [HttpPost("api/user/transfermoney")]
+
+        public IActionResult TransferMoney(int from, int to, double amount)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = connection;
+
+                MySqlTransaction transaction = connection.BeginTransaction();
+                command.Transaction = transaction;
+
+                try
+                {
+                    command.CommandText = "UPDATE users SET money = money - @amount WHERE id = @from";
+                    command.Parameters.AddWithValue("@amount", amount);
+                    command.Parameters.AddWithValue("@from", from);
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "UPDATE users SET money = money + @toAmount WHERE id = @to";
+                    command.Parameters.AddWithValue("@toAmount", amount);
+                    command.Parameters.AddWithValue("@to", to);
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    var exception = new { Message = ex.Message, StackTrace = ex.StackTrace };
+                    return StatusCode(500, exception);
+                }
+            }
+        }
+    }
+}
